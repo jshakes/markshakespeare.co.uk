9b644347e13859901e7ee4406fc641b3cf21f5a3
James Shakespeare <j@jshakespeare.com>
2018-01-22 22:01:58 -0500

    remove typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography.js',
-      },
-    },
-    {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-6245948-8',
