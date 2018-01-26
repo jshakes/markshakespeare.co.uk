e4ec0f790ca960213549fcb46f4bf2bc2bc7c4b6
James Shakespeare <j@jshakespeare.com>
2018-01-25 22:53:55 -0500

    artwork page text

--- src/pages/artwork.js
+++ src/pages/artwork.js
@@ -5,6 +5,7 @@ import Page from '../components/Page';
 const AboutPage = props => {
   return (
     <Page title="Artwork">
+      <p>Mark continues to paint and draw. A small selection of his artwork can be found below. For sales enquiries, please email markjshakespeare@gmail.com.</p>
       <ImageGrid />
     </Page>
   );  
