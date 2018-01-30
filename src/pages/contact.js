b4b5566e37c77bcfd08e5bfe8f320e300875fe03
James Shakespeare <j@jshakespeare.com>
2018-01-29 22:30:34 -0500

    add Linkedin

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,6 +4,7 @@ import Page from '../components/Page';
 const ContactPage = () => (
   <Page title="Contact">
     <p>For all enquiries please email markjshakespeare@gmail.com</p>
+    <p>You can also find me on <a href="https://www.linkedin.com/in/mark-shakespeare-567b496a/" target="_blank">LinkedIn</a></p>
   </Page>
 );
 
